8db70e5194231af0eb37e4697da4ba8921cc8041
BlooZE824 <66151506+BlooZE824@users.noreply.github.com>
2024-04-15 23:08:40 +0800

    change mysimple header setting

--- themes/mysimple/components/Header.js
+++ themes/mysimple/components/Header.js
@@ -1,6 +1,6 @@
 // import LazyImage from '@/components/LazyImage'
 import Link from 'next/link'
-import CONFIG from '../config'
+// import CONFIG from '../config'
 import SocialButton from './SocialButton'
 import { siteConfig } from '@/lib/config'
 
@@ -12,21 +12,20 @@ export default function Header (props) {
   // const { siteInfo } = props
 
   return (
-        <header className="text-center justify-between items-center px-6 bg-white h-20 dark:bg-black relative z-10">
-            <div className="float-none inline-block py-12">
+        <header className="flex justify-start item-center px-6 bg-white h-16 dark:bg-black relative z-10">
+            <div className="float-none inline-block py-3">
                 <Link href='/'>
                     {/* 可使用一张单图作为logo */}
-                    <div className='flex space-x-6'>
+                    <div className='flex space-x-6 justify-center items-center'>
                         <div className='flex-col flex justify-center'>
-                        <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
-                            <div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center' dangerouslySetInnerHTML={{ __html: siteConfig('SIMPLE_LOGO_DESCRIPTION', null, CONFIG) }} />
+                            <div className='text-2xl font-serif dark:text-white py-1  '>{siteConfig('AUTHOR')}</div>
+                        </div>
+                        {/* Social Media Link Button */}
+                        <div className='flex justify-center'>
+                            <SocialButton />
                         </div>
                     </div>
                 </Link>
-                {/* Social Media Link Button */}
-                <div className='flex justify-center'>
-                <SocialButton />
-                </div>
             </div>
         </header>
   )
